e0a525ef2d627975b34d2ecbd0de52c034d3d07b
Michael Boimistruck <michaelb_38@hotmail.com>
2020-01-07 18:25:58 -0500

    Update scrapper.py
    added file checking

--- scrapper.py
+++ scrapper.py
@@ -8,7 +8,6 @@ page, prices, cardName = ([] for i in range(3))
 
 file = open("prices.txt", "a") #append to file, "w" to overwrite
 URLFile = open("URLs.txt").readlines()
-URLFile.close()
 x = 0
 
 for each in URLFile:
@@ -17,10 +16,30 @@ for each in URLFile:
     if (soup.find('span', attrs={"class": "variant-short-info"}).get_text() == "Out of stock."):
         prices.append("Out of Stock")
     else:
-        prices.append(soup.find('span', attrs={"class": "regular price"}).get_text())
+        prices.append(soup.find('span', attrs={"class": "regular price"}).get_text()[5:])
+
     cardName.append(soup.find('h1', attrs={"class": "title"}).get_text())
+    try:
+        individualCards = open("library/"+cardName[x]+".txt", "r")
+        pulledPrice = individualCards.readlines()
+        pulledPriceString = "".join(pulledPrice)
+
+        if (pulledPriceString != prices[x]):
+            print(cardName[x] + " has changed price from " + pulledPriceString + " to " + prices[x])
+            individualCards.seek(0)
+            individualCards.truncate()
+            individualCards.write(prices[x])
+            individualCards.close()
+        else:
+            print("There was no price change for " + cardName[x] + " at the price of " + prices[x])
+    except:
+        individualCards = open("library/" + cardName[x] + ".txt", "w")
+        individualCards.write(prices[x])
+        individualCards.close()
+
     file.write(cardName[x] + " | " + prices[x] + " | " + today.strftime("%d/%m/%Y") + "\n")
     x += 1
 
 file.write("-------------------\n")
 file.close()
+input("Press any key to exit")
